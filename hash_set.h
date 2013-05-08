f8977353100f3bf3e81c223398dacc4e46800809
Avitella <urtashev@gmail.com>
2013-05-08 23:25:18 +0400

    hash_set

---
+++ hash_set.h
@@ -0,0 +1,40 @@
+#ifndef _HASH_SET_H
+#define _HASH_SET_H
+
+#include <vector>
+#include <algorithm>
+
+template<const int LIM = (int) 1e+6 + 7> 
+class hash_set {
+ private:
+  std::vector<long long> hash[LIM];
+  
+ public:
+  hash_set() {}
+  
+  inline int pos(long long h) {
+    return (h % LIM + LIM) % LIM;
+  }
+  
+  void insert(long long h) {
+    hash[pos(h)].push_back(h);
+  }
+  
+  bool find(long long h) {
+    std::vector<long long>::iterator first = hash[pos(h)].begin();
+    std::vector<long long>::iterator last = hash[pos(h)].end();
+    
+    return std::find(first, last, h) != last;
+  }
+  
+  void erase(long long h) {
+    hash[pos(h)].erase(h);
+  }
+  
+  void clear() {
+    for (int i = 0; i < LIM; i++)
+      hash[i].clear();
+  }
+};
+
+#endif
