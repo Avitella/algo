9d8cef97fbab9bf662416c93a98674ed82fabc9a
Avitella <urtashev@gmail.com>
2013-05-18 21:17:45 +0400

    new algo

---
+++ string_functions.h
@@ -0,0 +1,20 @@
+#ifndef _STRING_FUNCTIONS
+#define _STRING_FUNCTIONS
+
+#include <vector>
+#include <string>
+
+std::vector<size_t> prefix_function(const std::string& s) {
+  std::vector<size_t> prefix(s.length(), 0);
+  for (size_t i = 1; i < s.length(); ++i) {
+    size_t j = prefix[i - 1];
+    while (j > 0 && s[i] != s[j])
+      j = prefix[j - 1];
+    if (s[i] == s[j])
+      ++j;
+    prefix[i] = j;
+  }
+  return prefix;
+}
+
+#endif
---
+++ string_tokenizer.h
@@ -0,0 +1,91 @@
+#ifndef _STRING_TOKENIZER
+#define _STRING_TOKENIZER
+
+#include <queue>
+#include <string>
+#include <deque>
+#include "string_functions.h"
+
+class string_tokenizer {
+ public:
+  string_tokenizer(const std::string& s, const std::string& separator) {
+    std::string t = separator + '\0' + s;
+    std::vector<size_t> prefix = prefix_function(t);
+    std::vector<bool> sep_begin(t.length(), false);
+    for (size_t i = separator.length() + 1; i < prefix.size(); ++i) {
+      if (prefix[i] == separator.length()) {
+        sep_begin[i - separator.length() + 1] = true;
+      }
+    }
+    std::string add;
+    size_t i = separator.length() + 1;
+    while (i < prefix.size()) {
+      if (sep_begin[i]) {
+        if (!add.empty()) {
+          tokens.push(add);
+          add.clear();
+        }
+        i += separator.length();
+      } else {
+        add += t[i];
+        ++i;
+      }
+    }
+    if (!add.empty())
+      tokens.push(add);
+  }
+  
+  string_tokenizer(const std::string& s, const char& separator = ' ') {
+    std::string add;
+    for (auto c : s) {
+      if (c != separator) {
+        add += c;
+      } else if (!add.empty()) {
+        tokens.push(add);
+        add.clear();
+      }
+    }
+    if (!add.empty())
+      tokens.push(add);
+  }
+  
+  void push(std::string s, std::string separator) {
+    string_tokenizer(s, separator);
+  }
+  
+  void push(std::string s, char separator = ' ') {
+    string_tokenizer(s, separator);
+  }
+  
+  bool empty() {
+    return tokens.empty();
+  }
+  
+  size_t size() {
+    return tokens.size();
+  }
+  
+  std::string next_token() {
+    std::string res = tokens.front(); 
+    tokens.pop();
+    return res;
+  }
+  
+  std::string next_element() {
+    return tokens.front();
+  }
+  
+  void pop() {
+    tokens.pop();
+  }
+  
+  void clear() {
+    while (!tokens.empty())
+      tokens.pop();
+  }
+  
+ private:
+  std::queue< std::string, std::deque<std::string> > tokens;
+};
+
+#endif
