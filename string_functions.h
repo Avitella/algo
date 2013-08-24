7e88e9ef4a750b0f47a4c29ca87914ad130fd570
bdzl <vladb15@mail.ru>
2013-08-24 12:38:53 +0400

    optimization + z_function

--- string_functions.h
+++ string_functions.h
@@ -5,7 +5,7 @@
 #include <string>
 
 std::vector<size_t> prefix_function(const std::string& s) {
-  std::vector<size_t> prefix(s.length(), 0);
+  std::vector<size_t> prefix(s.length());
   size_t j = 0;
   for (size_t i = 1; i < s.length(); ++i) {
     while (j > 0 && s[i] != s[j])
@@ -15,6 +15,20 @@ std::vector<size_t> prefix_function(const std::string& s) {
     prefix[i] = j;
   }
   return prefix;
+}
+
+std::vector<size_t> z_function(const std::string& s) {
+  std::vector<size_t> z(s.length());
+  for (size_t i=1, l=0, r=0; i<s.length(); ++i) {
+    if (i <= r)
+      z[i] = std::min(z[i-l], r-i+1);
+    while (i+z[i]<s.length() && s[i+z[i]]==s[z[i]]) ++z[i];
+    if (i+z[i]-1 > r) {
+      l = i;
+      r = i+z[i]-1;
+    }
+  }
+  return z;
 }
 
 #endif
